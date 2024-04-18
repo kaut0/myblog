8c1b27e1567d7b269014295f185321db0fcfe84b
kaut0 <zilullah792@gmail.com>
2024-04-18 09:56:56 +0800

    font change

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -75,15 +75,15 @@ export default function NavbarComponent() {
         <Modal.Body>
           <div className="text-center">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400 font-sans">
               Yakin ingin keluar?
             </h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={() => handleLogOut()}>
-                <label className="text-white">{"Iya, Yakin"}</label>
+                <label className="text-white font-sans">{"Iya, Yakin"}</label>
               </Button>
               <Button color="gray" onClick={() => setOpenModal(false)}>
-                Tidak, batalkan
+                <label className="font-sans">Tidak, Batalkan</label>
               </Button>
             </div>
           </div>
